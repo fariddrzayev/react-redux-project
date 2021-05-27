63734cf57e72a4a79acf3b3b41d929d4717a8a1a
FaridR <Fr12345678>
2021-05-27 17:01:37 +0400

    f

--- src/components/LogContainer.js
+++ src/components/LogContainer.js
@@ -1,10 +1,41 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useLayoutEffect} from 'react'
 
 function LogContainer(props) {
 
     const [amountToTransfer, setAmountToTransfer] = useState(1);
     const [salary, setSalary] = useState(100);
     const [counter, setCounter] = useState(10);
+
+
+    const ArrayDep = () => {
+        const [randomNumber, setRandomNumber] = useState(0)
+        const [effectLogs, setEffectLogs] = useState([])
+      
+        useLayoutEffect(
+          () => {
+            setEffectLogs(prevEffectLogs => [...prevEffectLogs, 'effect fn has been invoked'])
+          },
+          [randomNumber]
+        )
+      
+        return (
+          <div>
+            <h1>{randomNumber}</h1>
+            <button
+              onClick={() => {
+                setRandomNumber(Math.random())
+              }}
+            >
+              Generate random number!
+            </button>
+            <div>
+              {effectLogs.map((effect, index) => (
+                <div key={index}>{'🍔'.repeat(index) + effect}</div>
+              ))}
+            </div>
+          </div>
+        )
+      }
 
     // const itemList = props.item.map(item =>
 
@@ -21,8 +52,22 @@ function LogContainer(props) {
     return (
         <div>
 
-            <div>
-                <button className="btn btn-primary">gg</button>
+            <div class="container">
+                <div class="row justify-content-md-center">
+                    <div class="col-md-4">
+                        <ul class="list-group">
+                            <li class="list-group-item">Cras justo odio</li>
+                            <li class="list-group-item">Dapibus ac facilisis in</li>
+                            <li class="list-group-item">Morbi leo risus</li>
+                            <li class="list-group-item">Porta ac consectetur ac</li>
+                            <li class="list-group-item">Vestibulum at eros</li>
+                        </ul>
+                    </div>
+                
+                </div>
+            </div>
+            <div class="col-md-3 justify-content-md-center ">
+                
             </div>
             
         </div>
@@ -30,3 +75,6 @@ function LogContainer(props) {
 }
 
 export default LogContainer
+
+
+
